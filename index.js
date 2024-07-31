6133c6635ca28917aa371ed371599dc888d0164c
+ <+>
2024-07-31 16:34:34 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(user, userEmail) {
+    const url = ('http://localhost:3000/users');
+    const data = {
+        name: user,
+        email: userEmail
+    };
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "accept": "application/json",
+        },
+        body: JSON.stringify(data),
+    };
+
+    return fetch(url, requestOptions)
+    .then(respond => respond.json())
+    .then(object => {
+    let firstBody = document.querySelector('body')
+    firstBody.append(object.id);
+    })
+    .catch(error => {
+        let firstError = document.querySelector('body')
+        firstError.append(error.message);
+    })
+    
+}
